6d05b7ade381034a4581ef5c0dbbba78b3ab510f
CAFETO <43516663+Quiidit@users.noreply.github.com>
2019-04-29 01:25:55 +0900

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
     render(){
         return (
           <div>
-          <button onClick = {() => {this.setState({name : 'Velopert'});}}>Click Me</button>
+          <button onClick = {() => {this.setState({name : 'Yeongjun'});}}>Click Me</button>
           <h1>Hello!! {this.state.name} </h1>
           </div>
         );
